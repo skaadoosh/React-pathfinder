c11846b09f827e76823746f131fac4b3a3c73141
skaadoosh <krishnakantdash007@gmail.com>
2022-07-13 12:18:16 +0530

    Updates

---
+++ precache-manifest.8f5942350969611628ccb4664ad747f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7385570499c4f291d7b8f4e43ca30305",
+    "url": "/React-pathfinder/index.html"
+  },
+  {
+    "revision": "137ab129f3b9198bb273",
+    "url": "/React-pathfinder/static/css/main.d2159a22.chunk.css"
+  },
+  {
+    "revision": "8d7c10ef2c5bf5fdb2a0",
+    "url": "/React-pathfinder/static/js/2.9af78dfd.chunk.js"
+  },
+  {
+    "revision": "137ab129f3b9198bb273",
+    "url": "/React-pathfinder/static/js/main.a42ec132.chunk.js"
+  },
+  {
+    "revision": "2ac78a5b558fae983481",
+    "url": "/React-pathfinder/static/js/runtime-main.a0f6a3d7.js"
+  }
+]);
